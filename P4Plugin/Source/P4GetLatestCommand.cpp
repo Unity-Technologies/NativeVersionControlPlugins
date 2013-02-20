f4b78c513242ffddcb7ab73879b0539211037e9a
Jonas Drewsen <jonasd@unity3d.com>
2013-02-20 09:07:49 +0100

    Make getlatest command return all fetched files and not just the assets listed

--- P4Plugin/Source/P4GetLatestCommand.cpp
+++ P4Plugin/Source/P4GetLatestCommand.cpp
@@ -1,18 +1,56 @@
 #include "Utility.h"
-#include "P4FileSetBaseCommand.h"
+#include "P4Command.h"
+#include "P4Utility.h"
 using namespace std;
 
 
-class P4GetLatestCommand : public P4FileSetBaseCommand
+class P4GetLatestCommand : public P4Command
 {
 public:
-	P4GetLatestCommand() : P4FileSetBaseCommand("getLatest", "sync") {}
+	P4GetLatestCommand() : P4Command("getLatest") {}
 	
+	virtual bool Run(P4Task& task, const CommandArgs& args)
+	{
+		incomingAssetList.clear();
+		ClearStatus();
+		Pipe().Log() << args[0] << "::Run()" << unityplugin::Endl;
+		
+		string cmd = "sync";
+		
+		VersionedAssetList assetList;
+		Pipe() >> assetList;
+		string paths = ResolvePaths(assetList, kPathWild | kPathRecursive);
+		
+		Pipe().Log() << "Paths resolved are: " << paths << unityplugin::Endl;
+		
+		if (paths.empty())
+		{
+			Pipe().ErrorLine("No paths in getlatest perforce command", MARemote);
+			Pipe().EndResponse();
+			return true;
+		}
+		
+		cmd += " " + paths;
+		
+		task.CommandRun(cmd, this);
+		Pipe() << GetStatus();
+		
+		// Stat the files to get the most recent state.
+		// This could probably be optimized by reading the output of the command better
+		P4Command* statusCommand = RunAndSendStatus(task, incomingAssetList);
+		Pipe() << statusCommand->GetStatus();
+		
+		// The OutputState and other callbacks will now output to stdout.
+		// We just wrap up the communication here.
+		Pipe().EndResponse();
+		return true;
+	}
+
 	virtual void HandleError( Error *err )
 	{
 		if ( err == 0 )
 			return;
-		
+				
 		StrBuf buf;
 		err->Fmt(&buf);
 		
@@ -25,5 +63,38 @@ public:
 		P4Command::HandleError(err);
 	}
 	
-} cGetLatest;
 
+	// Default handle of perforce info callbacks. Called by the default P4Command::Message() handler.
+	virtual void OutputInfo( char level, const char *data )
+	{
+		if (level != 48)
+		{
+			P4Command::OutputInfo(level, data);
+			return;
+		}
+
+		// format e.g.:
+		// //depot/P4Test/Assets/Lars.meta#2 - updating /Users/foobar/UnityProjects/PerforceTest/P4Test/Assets/Lars.meta
+		// //depot/P4Test/Assets/killme.txt#1 - added as /Users/foo....
+		// //depot/P4Test/Assets/killme.txt#1 - deleted as /Users/foo....
+
+		string d(data);
+		string::size_type i1 = d.find('#');
+		string::size_type i2 = d.find(' ', i1);
+		string rev = d.substr(i1+1, i2);
+
+		// Skip " updating " or " xxxxx as "
+		i1 = d.find(' ', i2+3);
+		i1++;
+
+		if (d.substr(i1, 3) == "as ")
+			i1 += 3;
+
+		string path = d.substr(i1);
+		
+		incomingAssetList.push_back(VersionedAsset(path, kSynced, rev));
+	}
+
+	VersionedAssetList incomingAssetList;
+
+} cGetLatest;
