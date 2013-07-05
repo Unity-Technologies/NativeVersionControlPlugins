68c153bb74d877dd5ab4b5903edeae9ea1717ec6
Jonas Drewsen <jonasd@unity3d.com>
2013-07-05 09:59:35 +0200

    Baseline handling

--- Test/run_tests.sh
+++ Test/run_tests.sh
@@ -4,6 +4,13 @@ SERVEREXEC=$1
 PLUGINEXEC=$2
 VERBOSE=$3
 TESTS=${*:4}
+PLUGINNAME=`basename $PLUGINEXEC`
+BASELINEDIR=Test/NewBaseline/$PLUGINNAME
+
+# Create the dir where log files are stored
+mkdir Library 2>/dev/null
+rm -rf $BASELINEDIR 2>&1 > /dev/null
+mkdir -p $BASELINEDIR
 
 run_tests()
 {
@@ -12,8 +19,16 @@ run_tests()
 	
 	for i in $TESTS ; do 
 		COUNT=$((COUNT+1))
-		$SERVEREXEC $PLUGINEXEC "$i" $VERBOSE; 
-		
+		NAME=`basename $i` ;
+		NEWBASELINE=$BASELINEDIR/$NAME ;
+		if [ "$VERBOSE" == "newbaseline" ] ; then
+			$SERVEREXEC $PLUGINEXEC "$i" $VERBOSE > "$NEWBASELINE" ;
+			echo "diff -Naur $i $NEWBASELINE" ;
+			diff -Naur "$i" "$NEWBASELINE" ;
+		else
+			$SERVEREXEC $PLUGINEXEC "$i" $VERBOSE;
+		fi
+
 		if [ "$?" == "0" ] ; then
 			SUCCESS=$((SUCCESS+1))
 		fi
