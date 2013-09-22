b8460d3bd9c4906fa1969d57b04331b38ecc8734
Emmanuel Horckmans <emmanuelh@unity3d.com>
2013-09-22 11:05:21 -0400

    AESPlugin + Fix UNIX

---
+++ AESPlugin/Source/AESPlugin_Posix.cpp
@@ -0,0 +1,10 @@
+#include "CommandLine.h"
+#include <iostream>
+#include <sstream>
+#include <string>
+
+// Program Entry point and set-up for Posix
+int main(int argc, char **argv)
+{
+    return 0;
+}
