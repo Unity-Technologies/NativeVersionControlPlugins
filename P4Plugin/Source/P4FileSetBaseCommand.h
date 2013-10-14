51f5a4e58cd9c8744de69bb2f5806dfc75a3a4d4
Jonas Drewsen <jonasd@unity3d.com>
2013-10-14 12:54:32 +0200

    Refactor filesetbase command. Redo the delete command to support reverting and handling of added and moved files. This used to be handled in Unity is now the responsibility of the plugin.

--- P4Plugin/Source/P4FileSetBaseCommand.h
+++ P4Plugin/Source/P4FileSetBaseCommand.h
@@ -8,8 +8,12 @@ class P4FileSetBaseCommand : public P4Command
 public:
 	P4FileSetBaseCommand(const char* name, const char* cmdstr = "");
 	virtual bool Run(P4Task& task, const CommandArgs& args);
+	virtual void ReadConnection() { }
 	virtual std::string SetupCommand(const CommandArgs& args);	
 	virtual int GetResolvePathFlags() const;
+protected:
+	bool Run(P4Task& task, const CommandArgs& args, const VersionedAssetList& assetList);
+
 private:
 	const std::string m_CmdStr;
 };
