018fd0aacf7952b5808996403ca4ff249776110a
Daniel Hompanera <daniel.hompanera@unity3d.com>
2022-07-26 17:38:25 +0200

    Update PerforcePlugin WinMain to receive -test param from TestServer

--- P4Plugin/Source/P4Plugin_WIN.cpp
+++ P4Plugin/Source/P4Plugin_WIN.cpp
@@ -8,6 +8,8 @@
 // Program Entry point and set-up for windows
 int __stdcall WinMain(HINSTANCE instance, HINSTANCE prevInstance, char* cmdLine, int cmdShow)
 {
+    std::string args(cmdLine);
+    const bool istestmode = args.find("-test") != std::string::npos ? true : false;
     P4Task task;
-    return task.Run();
+    return task.Run(istestmode);
 }
