c4ba22ad5a0a30a822f8906b0994db82c4a9365f
danielhompanera <daniel.hompanera@unity3d.com>
2022-08-01 18:19:46 +0200

    Refactor to P4SpecCommand with P4MFA to make things more readable

--- P4Plugin/Source/P4SpecCommand.cpp
+++ P4Plugin/Source/P4SpecCommand.cpp
@@ -1,6 +1,7 @@
 #include "P4Command.h"
 #include "P4Task.h"
 #include "POpen.h"
+#include "P4MFA.h"
 #include <sstream>
 #include <stdio.h>
 
@@ -23,57 +24,14 @@ public:
 			if (StatusContains(GetStatus(), "login2"))
 			{
 				ClearStatus();
-#if defined(_WINDOWS)
-				static const char* kMFAExePath = "\"C:\\Program Files\\Perforce\\HelixMFA.exe\"";
-#elif defined(_MACOS)
-				static const char* kMFAExePath = "/Applications/HelixMFA.app/Contents/MacOS/HelixMFA";
-#elif defined(_LINUX)
-				static const char* kMFAExePath = "helixmfa";
-#endif
-				Conn().VerboseLine(kMFAExePath);
-				try {
-					POpen proc = POpen(kMFAExePath + std::string(" ") + task.GetP4Port() + std::string(" ") + task.GetP4User());
-#if !defined(_WINDOWS)
-					std::string line = std::string();
-					bool helixmfa_found = false;
-					while (proc.ReadLine(line)) {
-						Conn().VerboseLine("HelixMFA: " + line);
-						//If first line reported by HelixMFA does not contain Authenticating, then we didnt find it
-						if (!helixmfa_found && line.find("Authenticating") == std::string::npos) {
-							const std::string notfound_error = "HelixMFA Authenticator could not be found. Download and install it to continue. https://www.perforce.com/downloads/helix-mfa-authenticator\n"
-							"If this error keeps showing after installation, you can try running HelixMFA manually with the following parameters: Port=" + task.GetP4Port() + ". User=" + task.GetP4User();
-							GetStatus().insert(VCSStatusItem(VCSSEV_Error, notfound_error));
-							Conn().Log().Notice() << GetStatusMessage() << Endl;
-						}
-						else {
-							helixmfa_found = true;
-						}
-					}; // Wait until stdout is closed
-					if (!helixmfa_found) return false;
-				}
-#else
-				}
-				catch (PluginException& pe)
+				std::string error;
+				if (!P4MFA::s_Singleton->WaitForHelixMFA(Conn(), task.GetP4Port(), task.GetP4User(), error))
 				{
-					if (StartsWith(pe.what(), "Process aborted "))
-					{
-						const std::string aborted_error = "The Helix MFA Authenticator did not complete. Run it again or use the \"p4 login\" or \"p4 login2\" CLI commands";
-						GetStatus().insert(VCSStatusItem(VCSSEV_Error, aborted_error));
-					}
-					else
-					{
-						const std::string notfound_error = "The Helix MFA Authenticator could not be found. Download and install it to continue. https://www.perforce.com/downloads/helix-mfa-authenticator";
-						GetStatus().insert(VCSStatusItem(VCSSEV_Error, notfound_error));
-					}
+					GetStatus().insert(VCSStatusItem(VCSSEV_Error, error));
 					Conn().Log().Notice() << GetStatusMessage() << Endl;
 					return false;
 				}
-#endif
-				catch (std::exception& e)
-				{
-					Conn().Log().Fatal() << "Unhandled exception: " << e.what() << Endl;
-					return false;
-				}
+				
 				//Try again the spec command so that we can get a value for m_Root
 				if (!task.CommandRun(cmd, this))
 				{
