2ccf6d4ba601e0837baddfc3327e621e15ce1019
Sébastien Rombauts <sebastien.rombauts@unity3d.com>
2025-07-29 10:03:46 +0200

    Update plugin version to 1.3.1-rc
    WIP Visual Studio 2019

--- P4Plugin/Source/P4PluginVersion.h
+++ P4Plugin/Source/P4PluginVersion.h
@@ -12,5 +12,6 @@
 // 1.2.1 2024/03/15 Fix login failure license about to expire
 //       2021.3.38f1, 2023.2.19f1, 6000.0.0b14
 // 1.3.0 2025/07/29 P4 API 2021.2 for Perforce Cloud
+// 1.3.1-rc WIP update for Visual Studio 2019
 
-#define PERFORCE_PLUGIN_VERSION "1.3.0"
+#define PERFORCE_PLUGIN_VERSION "1.3.1-rc"
