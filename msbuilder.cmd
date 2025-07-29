431f8ff55f5bf1c735c407f8e4f2858c0cad20d4
Sébastien Rombauts <sebastien.rombauts@unity3d.com>
2025-07-29 11:57:09 +0200

    Add hardcoded path to Microsoft Visual Studio\2019

--- msbuilder.cmd
+++ msbuilder.cmd
@@ -1,3 +1,6 @@
+if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\BuildTools\Common7\Tools\VsMSBuildCmd.bat" call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\BuildTools\Common7\Tools\VsMSBuildCmd.bat"
+if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Professional\Common7\Tools\VsMSBuildCmd.bat" call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Professional\Common7\Tools\VsMSBuildCmd.bat"
+
 for /f "usebackq delims=" %%i in (`call "%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -find VC\Auxiliary\Build\vcvarsall.bat`) do (
     call "%%i" x86
 )
